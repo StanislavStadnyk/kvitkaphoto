f43c7d87110826f7948f5ee7d89d05be0e112104
StaniSlavStadnyk <stanislav.stadnyk@gmail.com>
2023-10-05 09:52:32 +1100

    Replaced google font with font-face

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,29 @@ import { Head, Html, Main, NextScript } from 'next/document'
 const Document = () => (
   <Html lang="en">
     <Head>
+      <link
+        type="font/woff"
+        rel="preload"
+        href="/fonts/OpenSans-CondBold-webfont.woff"
+        as="font"
+        crossOrigin="true"
+      />
+      <link
+        type="font/woff"
+        rel="preload"
+        href="/fonts/OpenSans-CondLight-webfont.woff"
+        as="font"
+        crossOrigin="true"
+      />
+      <link
+        type="font/woff"
+        rel="preload"
+        href="/fonts/PTN57F-webfont.woff"
+        as="font"
+        crossOrigin="true"
+      />
       <link type="image/x-icon" rel="shortcut icon" href="/favicon.ico" />
-      <link href="/fonts/styles.css" rel="stylesheet" />
+      <link type="text/css" rel="stylesheet" href="/fonts/styles.css" />
     </Head>
 
     <body>
